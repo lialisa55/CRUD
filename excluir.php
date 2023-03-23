9d19f5165a3ff35c6025f958dbcfab37550d8906
unknown <nathalia.mota@etec.sp.gov.br>
2023-03-23 20:16:49 -0300

    added 'excluir.php', a form who will take the code to delete from the database

---
+++ excluir.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+</form>
+<form action="valida_excluir.php" method="POST">
+	<fieldset>
+	<table>
+	  <tr>
+	   <td> Digite o codigo do usuário: </td>
+	   <td><input size="15" name="codigo"></td>
+	  </tr>	
+	  <tr>
+	   <td colspan=2><input type="SUBMIT"  value="Alterar"></td>
+	  </tr>
+	  </table>
+	  </fieldset>
+	  
+	  </form>
+</body>
+</html>
